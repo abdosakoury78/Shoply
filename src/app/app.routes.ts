06f00c1f4315009b1113e94cbb8e32c1bcf24d4a
manarAhmed645 <mmony4544@gmail.com>
2026-07-22 21:09:54 +0200

    add routing

---
+++ src/app/app.routes.ts
@@ -0,0 +1,35 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { CartComponent } from './components/cart/cart.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
+
+
+export const routes: Routes = [
+    {
+        path: "", component: AuthLayoutComponent, children: [
+            { path: "", redirectTo:"signin" , pathMatch:"full" },
+            { path: "signin", component: SigninComponent, title: "Sign In" },
+            { path: "signup", component: SignupComponent, title: "Sign Up" },
+        ]
+    },
+    {
+        path: "", component: MainLayoutComponent, children: [
+            { path: "", redirectTo:"home" , pathMatch:"full" },
+            { path: "home", component: HomeComponent, title: 'Home' },
+            { path: "categories", component: CategoriesComponent, title: "Categories" },
+            { path: "brands", component: BrandsComponent, title: "Brands" },
+            { path: "products", component: ProductsComponent, title: "Products" },
+            { path: "cart", component: CartComponent, title: "Cart" },
+            { path: "wishlist", component:WishlistComponent, title: "WishList" },
+        ]
+    },
+    // { path: "**", component: NotFoundComponent, title: "Not Found" }
+];
